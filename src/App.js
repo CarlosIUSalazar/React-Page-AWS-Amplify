7365a3c0234a59cb72d712c7773762a7b072f7f0
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-07-29 18:29:24 +0900

    Added line to staging

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello my name is Carlos (Master Branch)</h1>
+      <h1>Hello my name is Carlos (Staging Branch)</h1>
       <h2>Welcome to my React Page on Amplify</h2>
       <h3>The current time is <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} /></h3>
       <img src="https://picsum.photos/600/400"></img> 
